b488aa6a26aa31e9e62c35bc6e481dc1862773f4
Yshin <m_yangjiabao@163.com>
2021-05-28 12:29:46 +0800

    调用实例

--- GraphicsMapApp/main.cpp
+++ GraphicsMapApp/main.cpp
@@ -1,9 +1,11 @@
-﻿#include <graphicsmap.h>
-#include <QApplication>
+﻿#include <QApplication>
 #include <QOpenGLWidget>
 #include <QGraphicsEllipseItem>
-#include "mappolygonitem.h"
-#include "mapellipseitem.h"
+#include <interactivemap.h>
+#include <mappolygonitem.h>
+#include <mapellipseitem.h>
+#include <maptrailitem.h>
+#include <mapobjectitem.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +14,7 @@ int main(int argc, char *argv[])
     fmt.setSamples(16);
     QSurfaceFormat::setDefaultFormat(fmt);
     auto scene = new QGraphicsScene;
-    MudMap map(scene);
+    InteractiveMap map(scene);
     map.setYInverted(true);
     map.setZoomLevel(10);
     map.centerOn({40.4015, 99.7895});
@@ -33,7 +35,7 @@ int main(int argc, char *argv[])
     QGeoCoordinate coord1 = coord0.atDistanceAndAzimuth(1e4, 90);
     QGeoCoordinate coord2 = coord1.atDistanceAndAzimuth(1e4, 180);
     QGeoCoordinate coord3 = coord0.atDistanceAndAzimuth(1e4, 180);
-    QList<QGeoCoordinate> coords;
+    QVector<QGeoCoordinate> coords;
     coords.append(coord0);
     coords.append(coord1);
     coords.append(coord2);
@@ -42,11 +44,38 @@ int main(int argc, char *argv[])
     item->setBrush(QBrush(Qt::red));
 //    item->setFlag(QGraphicsItem::ItemIsMovable, true);
     item->setEditable(true);
+//    item->setFlag(QGraphicsItem::ItemIsSelectable);
+    item->setSelected(true);
     //
     MapEllipseItem *ellipse = new MapEllipseItem;
+    ellipse->setEditable(false);
     scene->addItem(ellipse);
-    ellipse->setCenter({40.4014, 99.7895});
+    ellipse->setRect(QGeoCoordinate(40.4014, 99.7895), QGeoCoordinate(40.3454, 99.23));
     ellipse->setSize({5e4, 2e4});
+    ellipse->setBrush(QColor(255, 100, 100, 150));
+    ellipse->setEditable(true);
+//    ellipse->setFlag(QGraphicsItem::ItemIsSelectable);
+
+    //
+    MapTrailItem *trail = new MapTrailItem;
+    scene->addItem(trail);
+    trail->addCoordinate({40, 99});
+    trail->addCoordinate({41, 99});
+    trail->addCoordinate({41, 100});
+
+    //
+    QGraphicsEllipseItem *origin = new QGraphicsEllipseItem;
+    origin->setBrush(Qt::white);
+    origin->setRect(-2, -2, 4, 4);
+    origin->setRect(-2, -2, 4, 4);
+    origin->setFlag(QGraphicsItem::ItemIgnoresTransformations);
+    scene->addItem(origin);
+    MapObjectItem *obj = new MapObjectItem;
+    obj->setCoordinate({40,99});
+    obj->setColor(Qt::red);
+    scene->addItem(obj);
+
+    map.setOperator(new MapPolygonOperator);
 
     return a.exec();
 }
